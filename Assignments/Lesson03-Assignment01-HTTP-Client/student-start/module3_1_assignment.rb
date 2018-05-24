f7064e396cf04317f135168c2dab5fe637a1b33c
Bladefidz <gamerhacking@gmail.com>
2018-05-25 04:16:47 +0700

    Done module 3 assigment 1

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,15 @@
 require 'httparty'
 
+# Pwd: OTVacevcuhijWe7
+ENV['FOOD2FORK_KEY'] = "62f22f0e574df8dc52f358da6b5cb04f"
 class Recipe
+	include HTTParty
 
+	base_uri "http://food2fork.com/api"
+	default_params key: ENV['FOOD2FORK_KEY']
+	format :json
+
+	def self.for keyword
+		get("/search", query: { q: keyword })["recipes"]
+	end
 end
